8b50aa66e48742b68e234138e9521752b423347a
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-01-19 20:41:57 +0100

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -7,9 +7,9 @@ from store.models import Product, Customer, Collection, Order, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    # select products that has been ordered and sort by title
-    # filter product by looking up in OrderItem
-    # go to product table and select all with query_set id above:
-    query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    # query_set = Product.objects.prefetch_related('promotion').select_related('collection').all()
 
-    return render(request, 'hello.html', {'name': "Alex", 'products': list(query_set)})
+    # Get last 5 orders with their customer and items (incl product)
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+    return render(request, 'hello.html', {'name': "Alex", 'orders': list(query_set)})

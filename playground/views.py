3178e9d6d22f3ecc0c959dd0cee6f0fd01563945
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-01-22 16:12:10 +0100

    Custom Managers

--- playground/views.py
+++ playground/views.py
@@ -11,13 +11,6 @@ from tags.models import TaggedItem
 
 # Create your views here.
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
-
-    queryset = (TaggedItem.objects
-        .select_related('tag')
-        .filter(
-            content_type=content_type,
-            object_id=1
-    ))
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
     return render(request, 'hello.html', {'name': "Alex", 'tags': list(queryset)})

a5590427feacb34323fea354f76be13fab3b5318
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-01-25 17:22:56 +0100

    Editing Children using Inlines

--- store/admin.py
+++ store/admin.py
@@ -31,6 +31,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_select_related = ['collection']
     list_filter = ['collection', 'last_update', InventoryFilter]
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
@@ -51,11 +52,19 @@ class ProductAdmin(admin.ModelAdmin):
         )
 
 
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_display = ['id', 'placed_at', 'customer']
     list_per_page = 10
-    autocomplete_fields = ['customer']
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):

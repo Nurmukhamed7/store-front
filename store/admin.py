053fea791679376a2035635524f66162c11ad53d
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-01-25 21:12:17 +0100

    Using Generic Relations

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,13 @@
 from urllib.parse import urlencode
 
 from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models import QuerySet
 from django.db.models.aggregates import Count
 from django.utils.html import format_html
 from django.urls import reverse
 
+from tags.models import TaggedItem
 from . import models
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -19,8 +21,13 @@ class InventoryFilter(admin.SimpleListFilter):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
 
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
     prepopulated_fields = {
         "slug": ["title"]
     }

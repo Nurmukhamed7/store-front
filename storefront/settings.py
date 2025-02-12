71624ad6d49e28361db9d10924347eda9bc2f7d1
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-02-12 23:09:59 +0100

    deleted pycache, added .env, updated settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,7 +99,7 @@ DATABASES = {
         'NAME': 'storefront',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'Harvard!379',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
